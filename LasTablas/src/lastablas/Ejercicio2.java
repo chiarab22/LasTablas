166e82c1ab2bccb92bd5a1bb2b1a4c1434d217ae
Patriesc <patricia.sanz.escurin@gmail.com>
2022-02-28 10:10:36 +0100

    Creación carpeta de código

---
+++ LasTablas/src/lastablas/Ejercicio2.java
@@ -0,0 +1,14 @@
+package lastablas;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+    private int[] contador = new int[10];
+    private ArrayList listaNum = new ArrayList();
+
+    public void numOcurrencias(Scanner teclado){
+
+    }
+}
---
+++ LasTablas/src/lastablas/Main.java
@@ -0,0 +1,71 @@
+package lastablas;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int option;
+
+        do {
+            System.out.println("\n***************************");
+            System.out.println("Elija una de las opciones: ");
+            menu();
+            option = teclado.nextInt();
+            switch (option) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+
+
+            }
+
+        } while (option != 11);
+        System.out.println("¡Muchas gracias!");
+    }
+
+    private static void menu() {
+
+        System.out.println("1. Despegue inmediato");
+        System.out.println("2. Número de ocurrencias");
+        System.out.println("3. Nota media (versión 4)");
+        System.out.println("4. Máquina de votar");
+        System.out.println("5. Palíndromo");
+        System.out.println("6. ¿Qué hace?");
+        System.out.println("7. Matriz");
+        System.out.println("8. Micro guerra de barcos");
+        System.out.println("9. Tres en raya");
+        System.out.println("10.Registro de Empleados");
+        System.out.println("11. Salir");
+
+    }
+
+    }
+
