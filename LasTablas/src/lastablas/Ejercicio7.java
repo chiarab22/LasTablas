23546b6aa39fb25305d3746935876c7e09c667c4
Patriesc <patricia.sanz.escurin@gmail.com>
2022-03-02 09:19:50 +0100

    Ejercicio7 corregido

--- LasTablas/src/lastablas/Ejercicio7.java
+++ LasTablas/src/lastablas/Ejercicio7.java
@@ -12,6 +12,7 @@ public void imprimirTabla(){
     for (int i = 0; i < tabla.length; i++) {
         for (int j = 0; j < tabla.length; j++) {
             tabla[i][j] = caracter1;
+            caracter1 = (char) (r.nextInt(26) + 'a');
 
             System.out.print(tabla[i][j]);
         }
