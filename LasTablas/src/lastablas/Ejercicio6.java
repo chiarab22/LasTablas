bace3736955e85644f02e6b2c4b810b50f5816c6
Patriesc <patricia.sanz.escurin@gmail.com>
2022-03-02 09:10:08 +0100

    Actualización ejercicio6

---
+++ LasTablas/src/lastablas/Ejercicio6.java
@@ -0,0 +1,16 @@
+package lastablas;
+
+public class Ejercicio6 {
+    private final int TAMAÑO = 3;
+    private int i, j, val;
+    int[][] tab2d = new int[TAMAÑO][TAMAÑO];
+
+    public void queHace(){
+        val = 1;
+        for (int j = 0; j < TAMAÑO; j++) {
+            for (int i = 0; i < TAMAÑO; i++) {
+              tab2d[j][i] = val;
+            }
+        }
+    }
+}
--- LasTablas/src/lastablas/Main.java
+++ LasTablas/src/lastablas/Main.java
@@ -11,6 +11,7 @@ public class Main {
         Ejercicio3 ejercicio3 = new Ejercicio3();
         Ejercicio4 ejercicio4 = new Ejercicio4();
         Ejercicio5 ejercicio5 = new Ejercicio5();
+        Ejercicio6 ejercicio6 = new Ejercicio6();
         Ejercicio7 ejercicio7 = new Ejercicio7();
         Ejercicio10 ejercicio10 = new Ejercicio10();
 
@@ -36,6 +37,7 @@ public class Main {
                      ejercicio5.imprimirPalindromo();
                     break;
                 case 6:
+                    ejercicio6.queHace();
                     break;
                 case 7:
                     ejercicio7.imprimirTabla();
